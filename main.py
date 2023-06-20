deebf476daefc19c19a4f3f2effcabc67ba16338
hyewon-han <hw1635@hanmail.net>
2023-06-20 18:10:49 +0900

    create

---
+++ main.py
@@ -0,0 +1,7 @@
+from requests import get
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+print(response.text)
