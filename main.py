cde4889826a8ce6046a76fff15465ae48b3b911d
hyewon-han <hw1635@hanmail.net>
2023-06-23 15:54:39 +0900

    create wwr.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
 
 response = get(f"{base_url}{search_term}")
-print(response.text)
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    results = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs")
+    for jobs_section in jobs:
+        job_posts = jobs_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']  # 나중에 이 링크를 엑셀 파일에 저장하기 위함
+            company, kind, region = anchor.find_all('span', class_="company")
+
+            title = anchor.find('span', class_='title')
+
+            job_data = {
+                'link': f"https://weworkremotely.com{link}",
+                'company': company.string,
+                'region': region.string,
+                'position': title.string
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
+        print("/////")
